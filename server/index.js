e3ec400685c8fee2a3d31d53c107528945b4cfb6
roeechaimo <roee84@gmail.com>
2017-10-20 10:12:54 +0300

    add socket and page structure

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
 var indexFile = path.join(__dirname, '../', 'index.html');
 
@@ -10,6 +11,15 @@ app.get('/', function(req, res) {
   res.sendFile(indexFile);
 });
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+  });
+});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
